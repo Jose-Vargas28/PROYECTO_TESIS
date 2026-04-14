8ecbb6eeea767527a485646bf5a0ba359c0999fb
Jose Vargas <josemvargas.28@gmail.com>
2026-04-14 15:36:20 -0500

    Primer commit - estructura del proyecto

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+console.log("iniciando servidor....")
+
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get("/",(req, res) =>{
+    res.send("API funcionando")
+
+});
+
+app.listen(3000, () =>{
+    console.log("Servidor corriendo en puerto 3000")
+
+})
